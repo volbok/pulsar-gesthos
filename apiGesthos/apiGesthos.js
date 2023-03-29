132fc660cc4359926b3475deb17d40974291cf06
Rodrigo Lessa <rodrigo.lessa@ghap.com.br>
2023-03-28 22:51:18 -0300

    CORRIGINDO INTERNAÇÕES

--- apiGesthos/apiGesthos.js
+++ apiGesthos/apiGesthos.js
@@ -1,42 +1,193 @@
 const axios = require("axios");
 
-const criandoJson = () => {
+let html = 'https://pulasr-gesthos-api.herokuapp.com/';
+// let html = 'http://localhost:3333/'
+
+const criandoJsonAtendimento = () => {
   console.log('ENVIADO ARRAY COM JSONS');
   var obj =
   {
+    "credenciais":
+    {
+      "empresa": "13.025.354/0001-32",
+      "usuario": "AABBCCDD",
+      "password": "AABBCCDD"
+    },
     "pacientes": [
       {
         "internacao": {
-          "data": "05/02/2023",
-          "hora": "00:21:48",
-          "prontuario": "304574",
-          "atendimento": "1475486",
-          "paciente": "LENI APARECIDA DE SOUZA MIGUEL",
-          "sexo": "F",
-          "nascimento": "28/10/1960",
-          "unidadeinternacao": "C. T. I.  03",
-          "leito": "19"
-        }
+          "data": "01/01/2023",
+          "hora": "13:40:00",
+          "prontuario": "111",
+          "atendimento": "111",
+          "paciente": "PACIENTE 111",
+          "sexo": "M",
+          "nascimento": "01/01/1911",
+          "unidadeinternacao": "C. T. I.  01",
+          "leito": "21"
+        },
+      },
+      {
+        "alta": {
+          "data": "01/01/2023",
+          "hora": "02:30:00",
+          "prontuario": "111",
+          "atendimento": "111",
+          "paciente": "NOVO PACIENTE 111",
+          "sexo": "M",
+          "nascimento": "01/01/1911",
+          "unidadeinternacao": "C. T. I.  01",
+          "leito": "01"
+        },
+      },
+      {
+        "alta": {
+          "data": "02/02/2023",
+          "hora": "01:30:00",
+          "prontuario": "222",
+          "atendimento": "222",
+          "paciente": "PACIENTE 222",
+          "sexo": "M",
+          "nascimento": "01/01/1922",
+          "unidadeinternacao": "C. T. I.  01",
+          "leito": "02"
+        },
+      },
+      {
+        "alta": {
+          "data": "03/03/2023",
+          "hora": "09:56:00",
+          "prontuario": "333",
+          "atendimento": "333",
+          "paciente": "PACIENTE 333",
+          "sexo": "M",
+          "nascimento": "01/01/1933",
+          "unidadeinternacao": "C. T. I.  01",
+          "leito": "03"
+        },
       },
       {
         "internacao": {
-          "data": "05/02/2023",
-          "hora": "01:37:04",
-          "prontuario": "340012",
-          "atendimento": "1487506",
-          "paciente": "ANA PAULA FERNANDES BOLA",
-          "sexo": "F",
-          "nascimento": "02/10/1980",
+          "data": "03/03/2023",
+          "hora": "09:30:00",
+          "prontuario": "333",
+          "atendimento": "333",
+          "paciente": "PACIENTE 333",
+          "sexo": "M",
+          "nascimento": "01/01/1933",
           "unidadeinternacao": "C. T. I.  01",
-          "leito": "10"
-        }
+          "leito": "03"
+        },
       },
+      {
+        "alta": {
+          "data": "03/03/2023",
+          "hora": "09:33:00",
+          "prontuario": "333",
+          "atendimento": "333",
+          "paciente": "PACIENTE 333",
+          "sexo": "M",
+          "nascimento": "01/01/1933",
+          "unidadeinternacao": "C. T. I.  01",
+          "leito": "03"
+        },
+      }
     ]
   }
-  axios.post('https://pulsar-gesthos-api.up.railway.app/gesthos_atendimentos', obj).then(() => {
+  axios.post(html + 'gesthos_atendimentos', obj).then(() => {
+    console.log('ENVIADO! ' + JSON.stringify(obj));
   }).catch((err) => console.log(err));
 }
 
+const criandoJsonAssistencial = () => {
+  console.log('ENVIADO ARRAY COM JSONS');
+  var obj =
+  {
+    "credenciais":
+    {
+      "empresa": "13.025.354/0001-32",
+      "usuario": "AABBCCDD",
+      "password": "AABBCCDD"
+    },
+    "registro": [
+      {
+        "documento": {
+          "data": "01/03/2023",
+          "hora": "00:03:06",
+          "prontuario": "111",
+          "atendimento": "111",
+          "grupo": "01 - GRUPO DADOS VITAIS E CONTROLES",
+          "item": "0101 - PAS",
+          "valor": "A��O, ARQU�TIPO",
+        }
+      }
+    ]
+  }
+
+  axios.post(html + 'gesthos_assistencial', obj).then(() => {
+    console.log('ENVIADO! ' + JSON.stringify(obj));
+  }).catch((err) => console.log(err));
+}
+
+/*
+{
+        "documento": {
+          "data": "09/03/2023",
+          "hora": "00:20:06",
+          "prontuario": "33671",
+          "atendimento": "1494404",
+          "grupo": "05 - ANAMNESE E EVOLUCOES",
+          "item": "0503 - ANAMNESE MEDICACOES DE USO DOMICILIAR",
+          "valor": "TESTANDO CARACTETRES: AÇÃO, ÚLCERA, ARQUÉTIPO, ÍNDIO. SUCÇÃO.",
+        }
+      },
+      {
+        "documento": {
+          "data": "01/03/2023",
+          "hora": "00:03:06",
+          "prontuario": "111",
+          "atendimento": "111",
+          "grupo": "01 - GRUPO DADOS VITAIS E CONTROLES",
+          "item": "0101 - PAS",
+          "valor": "120",
+        }
+      },
+      {
+        "documento": {
+          "data": "01/03/2023",
+          "hora": "00:03:06",
+          "prontuario": "111",
+          "atendimento": "111",
+          "grupo": "01 - GRUPO DADOS VITAIS E CONTROLES",
+          "item": "0102 - PAD",
+          "valor": "80",
+        }
+      },
+      {
+        "documento": {
+          "data": "01/03/2023",
+          "hora": "00:03:06",
+          "prontuario": "111",
+          "atendimento": "111",
+          "grupo": "01 - GRUPO DADOS VITAIS E CONTROLES",
+          "item": "0103 - FC",
+          "valor": "74",
+        }
+      },
+      {
+        "documento": {
+          "data": "01/03/2023",
+          "hora": "00:03:06",
+          "prontuario": "111",
+          "atendimento": "111",
+          "grupo": "01 - GRUPO DADOS VITAIS E CONTROLES",
+          "item": "0104 - FR",
+          "valor": "18",
+        }
+      },
+*/
+
 setInterval(() => {
-  criandoJson();
-}, 10000);
+  // criandoJsonAtendimento();
+  criandoJsonAssistencial();
+}, 5000);
